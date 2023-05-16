6bfba34dacea3cb7150384a52c8c65dfcb6ff7b9
UmashankarChauhan <umashankarchauhan9@gmail.com>
2023-05-16 17:28:18 +0530

    updated path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/Home";
-import About from "./pages/about/About";
-import Contact from "./pages/contact/Contact";
-import Portfolio from "./pages/portfolio/Portfolio";
 import Navbar from "./components/Navbar";
 import Themes from "./components/Themes";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
+import Portfolio from "./pages/portfolio/Portfolio";
 // import emailjs from "emailjs-com";
 
 // emailjs.init("B4PwQa9zolbruMUDj");
